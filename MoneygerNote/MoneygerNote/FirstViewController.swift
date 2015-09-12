bc80772a0c4aee15a716fe290229c326c40d505f
yoat <yoatnic@me.com>
2015-09-12 19:09:30 +0900

    Create empty Xcode project

---
+++ MoneygerNote/MoneygerNote/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MoneygerNote
+//
+//  Created by Takaaki Yoshioka on 2015/09/12.
+//  Copyright © 2015年 JBS. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
